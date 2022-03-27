0bb54f1b3377416cae6fd666764feef42b37ed2f
Dmitry-Konovalov <89130809848@mail.ru>
2022-03-27 13:00:05 +0700

    27/03/22 all completed
    Signed-off-by: Dmitry-Konovalov <89130809848@mail.ru>
    
    # Conflicts:
    #	index.html
    #	style.css

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ $(function(){
     let group1H = group1.innerHeight();
     let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll", function(){
+    $(window).on("scroll load", function(){
         scrollPos = $(this).scrollTop();
 
-        if(scrollPos > group1H){
+        if(scrollPos > group1H - 250){
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
     });
-
     console.log(group1H)
     header.addClass("fixed");
 
-});
+/*------======scroll======------*/
+
+    $("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+        let blockID = $(this).data("scroll");
+        let blockOffset = $(blockID).offset().top;
+        console.log(blockOffset);
+        console.log(blockID)
+
+        if(blockID == "#group2"){
+            $("html, body").animate({
+                scrollTop: blockOffset - 100}, 600);
+        } else {
+            $("html, body").animate({
+                scrollTop: blockOffset}, 600);
+            }
+        })
+    });
+
